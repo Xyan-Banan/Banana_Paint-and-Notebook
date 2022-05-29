9fdbdd27c1b601bb6b6aad05a63c25403c5e8cd3
Mike <20vampir11@gmail.com>
2022-05-29 14:11:54 +0300

    Добавьте файлы проекта.

---
+++ FilesForms/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace FilesForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openImageBtn = new System.Windows.Forms.Button();
+            this.saveImageBtn = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openTextBtn = new System.Windows.Forms.Button();
+            this.saveTextBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorBlack = new System.Windows.Forms.PictureBox();
+            this.colorRed = new System.Windows.Forms.PictureBox();
+            this.colorBlue = new System.Windows.Forms.PictureBox();
+            this.colorGreen = new System.Windows.Forms.PictureBox();
+            this.saveTextAsBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBlack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorGreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openImageBtn
+            // 
+            this.openImageBtn.Location = new System.Drawing.Point(152, 303);
+            this.openImageBtn.Name = "openImageBtn";
+            this.openImageBtn.Size = new System.Drawing.Size(128, 23);
+            this.openImageBtn.TabIndex = 0;
+            this.openImageBtn.Text = "Открыть картинку";
+            this.openImageBtn.UseVisualStyleBackColor = true;
+            this.openImageBtn.Click += new System.EventHandler(this.openImageBtn_Click);
+            // 
+            // saveImageBtn
+            // 
+            this.saveImageBtn.Location = new System.Drawing.Point(152, 346);
+            this.saveImageBtn.Name = "saveImageBtn";
+            this.saveImageBtn.Size = new System.Drawing.Size(128, 23);
+            this.saveImageBtn.TabIndex = 1;
+            this.saveImageBtn.Text = "Сохранить картинку";
+            this.saveImageBtn.UseVisualStyleBackColor = true;
+            this.saveImageBtn.Click += new System.EventHandler(this.saveImageBtn_Click);
+            // 
+            // pb
+            // 
+            this.pb.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb.Location = new System.Drawing.Point(85, 12);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(262, 247);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb.TabIndex = 2;
+            this.pb.TabStop = false;
+            this.pb.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pb.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pb.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(353, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(252, 247);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openTextBtn
+            // 
+            this.openTextBtn.Location = new System.Drawing.Point(409, 303);
+            this.openTextBtn.Name = "openTextBtn";
+            this.openTextBtn.Size = new System.Drawing.Size(128, 23);
+            this.openTextBtn.TabIndex = 0;
+            this.openTextBtn.Text = "Открыть текст";
+            this.openTextBtn.UseVisualStyleBackColor = true;
+            this.openTextBtn.Click += new System.EventHandler(this.openTextBtn_Click);
+            // 
+            // saveTextBtn
+            // 
+            this.saveTextBtn.Location = new System.Drawing.Point(409, 346);
+            this.saveTextBtn.Name = "saveTextBtn";
+            this.saveTextBtn.Size = new System.Drawing.Size(128, 23);
+            this.saveTextBtn.TabIndex = 1;
+            this.saveTextBtn.Text = "Сохранить текст";
+            this.saveTextBtn.UseVisualStyleBackColor = true;
+            this.saveTextBtn.Click += new System.EventHandler(this.saveTextBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // colorBlack
+            // 
+            this.colorBlack.BackColor = System.Drawing.Color.Black;
+            this.colorBlack.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.colorBlack.Location = new System.Drawing.Point(12, 60);
+            this.colorBlack.Name = "colorBlack";
+            this.colorBlack.Size = new System.Drawing.Size(66, 20);
+            this.colorBlack.TabIndex = 5;
+            this.colorBlack.TabStop = false;
+            this.colorBlack.Click += new System.EventHandler(this.setPen);
+            // 
+            // colorRed
+            // 
+            this.colorRed.BackColor = System.Drawing.Color.Red;
+            this.colorRed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorRed.Location = new System.Drawing.Point(12, 86);
+            this.colorRed.Name = "colorRed";
+            this.colorRed.Size = new System.Drawing.Size(66, 20);
+            this.colorRed.TabIndex = 5;
+            this.colorRed.TabStop = false;
+            this.colorRed.Click += new System.EventHandler(this.setPen);
+            // 
+            // colorBlue
+            // 
+            this.colorBlue.BackColor = System.Drawing.Color.Blue;
+            this.colorBlue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorBlue.Location = new System.Drawing.Point(13, 112);
+            this.colorBlue.Name = "colorBlue";
+            this.colorBlue.Size = new System.Drawing.Size(66, 20);
+            this.colorBlue.TabIndex = 5;
+            this.colorBlue.TabStop = false;
+            this.colorBlue.Click += new System.EventHandler(this.setPen);
+            // 
+            // colorGreen
+            // 
+            this.colorGreen.BackColor = System.Drawing.Color.Lime;
+            this.colorGreen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorGreen.Location = new System.Drawing.Point(13, 138);
+            this.colorGreen.Name = "colorGreen";
+            this.colorGreen.Size = new System.Drawing.Size(66, 20);
+            this.colorGreen.TabIndex = 5;
+            this.colorGreen.TabStop = false;
+            this.colorGreen.Click += new System.EventHandler(this.setPen);
+            // 
+            // saveTextAsBtn
+            // 
+            this.saveTextAsBtn.Location = new System.Drawing.Point(392, 386);
+            this.saveTextAsBtn.Name = "saveTextAsBtn";
+            this.saveTextAsBtn.Size = new System.Drawing.Size(160, 23);
+            this.saveTextAsBtn.TabIndex = 6;
+            this.saveTextAsBtn.Text = "Сохранить текст как...";
+            this.saveTextAsBtn.UseVisualStyleBackColor = true;
+            this.saveTextAsBtn.Click += new System.EventHandler(this.saveTextAsBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 450);
+            this.Controls.Add(this.saveTextAsBtn);
+            this.Controls.Add(this.colorGreen);
+            this.Controls.Add(this.colorBlue);
+            this.Controls.Add(this.colorRed);
+            this.Controls.Add(this.colorBlack);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.saveTextBtn);
+            this.Controls.Add(this.openTextBtn);
+            this.Controls.Add(this.saveImageBtn);
+            this.Controls.Add(this.openImageBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBlack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorGreen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openImageBtn;
+        private System.Windows.Forms.Button saveImageBtn;
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button openTextBtn;
+        private System.Windows.Forms.Button saveTextBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox colorBlack;
+        private System.Windows.Forms.PictureBox colorRed;
+        private System.Windows.Forms.PictureBox colorBlue;
+        private System.Windows.Forms.PictureBox colorGreen;
+        private System.Windows.Forms.Button saveTextAsBtn;
+    }
+}
+
---
+++ FilesForms/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace FilesForms
+{
+    public partial class Form1 : Form
+    {
+        string imageName;
+        Pen pen;                //ручка, которой мы рисуем
+        PictureBox curColor;    //текущий цвет
+        List<Point> points;     //точки для отрисовки линии
+        //--------------
+        string textFile;
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Brushes.Black, 1);
+            curColor = colorBlack;
+            points = new List<Point>();
+            pb.Image = new Bitmap(pb.Width, pb.Height);
+        }
+        
+
+        private void openImageBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "Image files(*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png; *.gif; *.bmp",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pb.Image = Image.FromFile(dialog.FileName);
+                    imageName = dialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка! Невозможно прочитать файл с диска! " +
+                        "Сообщение об ошибке: " + ex.Message);
+                }
+            }
+        }
+
+        private void saveImageBtn_Click(object sender, EventArgs e)
+        {
+            if (pb.Image == null) return;
+
+            if (imageName == null)
+                imageName = "untitled.bmp";
+
+            SaveFileDialog saveImage = new SaveFileDialog
+            {
+                FileName = Path.GetFileNameWithoutExtension(imageName),
+                DefaultExt = Path.GetExtension(imageName).Trim(new char[] { ' ', '.' }),
+                ValidateNames = true,
+                Filter = "Bitmap Image (.bmp)|*.bmp|Gif Image (.gif)|*.gif|JPEG Image (.jpg)|*.jpg|Png Image (.png)|*.png"
+            };
+
+            if(saveImage.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(pb.Image);
+                bmp.Save(saveImage.FileName);
+            }
+        }
+
+        private Point transform(Point original)
+        {
+            float wRatio = (float)pb.Image.Width / pb.Width;
+            float hRatio = (float)pb.Image.Height / pb.Height;
+            return new Point
+            {
+                X = (int)(original.X * wRatio),
+                Y = (int)(original.Y * hRatio)
+            };
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine($"{e.X} {e.Y} {e.Button} {points.Count}");
+            Console.WriteLine($"pb: {pb.Size}; Image: {pb.Image.Size}");
+            Console.WriteLine($"original: {e.Location}; \ttransformed: {transform(e.Location)}");
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics canvas = Graphics.FromImage(pb.Image))
+                {
+                    points.Add(transform(e.Location));
+                    canvas.SmoothingMode = SmoothingMode.AntiAlias;
+                    canvas.DrawCurve(pen, points.ToArray(), 0.1f);
+                }
+                pb.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                points.Add(transform(e.Location));
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                points.Clear();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float width = (float)Convert.ToDouble(textBox1.Text);
+                if (width < 1)
+                    width = 1;
+                else if (width > 20)
+                    width = 20;
+
+                pen.Width = width;
+                textBox1.Text = width.ToString();
+            }
+            catch (Exception)
+            {
+                textBox1.Text = pen.Width.ToString();
+            }
+        }
+
+        void setPen(PictureBox color)
+        {
+            pen.Color = color.BackColor;
+            curColor.BorderStyle = BorderStyle.None;
+            curColor = color;
+            curColor.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void setPen(object sender, EventArgs e)
+        {
+            setPen((PictureBox)sender);
+        }
+
+        private void openTextBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "Text files (*.txt, *.json) | *.txt; *.json | All files (*.*) | *.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    richTextBox1.Text = File.ReadAllText(dialog.FileName);
+                    textFile = dialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка! Невозможно прочитать файл с диска! Сообщение об ошибке: " + ex.Message);
+                }
+
+            }
+
+        }
+
+        private void saveTextBtn_Click(object sender, EventArgs e)
+        {
+            if (textFile == null)
+            {
+                SaveFileDialog saveText = new SaveFileDialog();
+                saveText.FileName = "untitled";
+                saveText.DefaultExt = ".txt";
+                saveText.ValidateNames = true;
+                saveText.Filter = "Text files (*.txt, *.json) | *.txt; *.json | All files (*.*) | *.*";
+
+                if (saveText.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveText.FileName, richTextBox1.Text);
+                    textFile = saveText.FileName;
+                }
+            }
+            else
+            {
+                File.WriteAllText(textFile, richTextBox1.Text);
+            }
+        }
+
+        private void saveTextAsBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveText = new SaveFileDialog();
+            saveText.FileName = "untitled";
+            saveText.DefaultExt = ".txt";
+            saveText.ValidateNames = true;
+            saveText.Filter = "Text files (*.txt, *.json) | *.txt; *.json | All files (*.*) | *.*";
+
+            if (saveText.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveText.FileName, richTextBox1.Text);
+                textFile = saveText.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
